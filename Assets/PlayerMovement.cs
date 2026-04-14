9f5d5bb119ae73674614aadbd5e27db4e54e6e23
lxvenderteaa <lxvenderteaa.jp@gmail.com>
2026-04-14 10:21:06 +0100

    14/04/26 1
    fixed movement
    remember to make it 4 way

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,36 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed=5f
-    private RigidBody2D rb;
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
     private Vector2 moveInput;
 
     void Start()
     {
-        rb = Getomponent<RigidBody2D>();
-        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        rb.velocity = moveInput * moveSpeed;
+        rb.linearVelocity = moveInput * moveSpeed;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        animator.SetBool("isWalking", true);
-        if (context.canceled)
-        {
-            animator.SetBool("isWalking", false);
-            animator.SetFloat("LastInputX", moveInput.x);
-            animator.SetFloat("LastInputY", moveInput.y);
-        }
-
         moveInput = context.ReadValue<Vector2>();
-        animator.Setfloat("InputX", moveInput.x);
-        animator.Setfloat("InputY", moveInput.y);
     }
 }
